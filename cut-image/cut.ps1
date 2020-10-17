11f145d95e8b53979b140acea3e949c7f086e621
Eccenux <eccenux+git@gmail.com>
2020-10-17 03:44:38 +0200

    half size

--- cut-image/cut.ps1
+++ cut-image/cut.ps1
@@ -9,13 +9,17 @@
 
 ##
 # Settings
+##
 $irfan="C:\Program Files (x86)\IrfanView\i_view32.exe"
-$h=3396		# height of largest column
+$h=4000		# height of largest column (for smaller images)
 $top=100	# top bar height
 $w=490		# width of the widest image
 $gap=10		# gap between columns
 $cols=19	# number of columns
+$sourceImage="raw.jpg"
 
+##
+# Internal settings
 ##
 # total width of a column
 $totalW = $w + $gap
@@ -28,7 +32,15 @@ $totalW = $w + $gap
 for($colNumber = 1; $colNumber -lt $cols + 1; $colNumber++)
 {
 	$startX=($colNumber - 1) * $totalW
-	$parmeters="raw.jpg /crop=($startX,$top,$w,$h) /convert=.\out\col_$colNumber.jpg"
+	$base="$sourceImage /crop=($startX,$top,$w,$h)"
+	
+	# full size
+	$parmeters="$base /convert=.\out\col_$colNumber.jpg"
 	Write-Output "$parmeters"
+	& "$irfan" "$parmeters"
+
+	# ~half size
+	$resize="/resize_short=200 /aspectratio /resample"
+	$parmeters="$base $resize /convert=.\out\small_col_$colNumber.jpg"
 	& "$irfan" "$parmeters"
 }
