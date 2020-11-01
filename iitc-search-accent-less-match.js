0f9b4d85f453ecf82bfa13df7c5b957984768189
Eccenux <eccenux+git@gmail.com>
2020-11-01 14:33:49 +0100

    IitcAccentLessSearch starter
    copy from exact match

--- iitc-search-accent-less-match.js
+++ iitc-search-accent-less-match.js
@@ -1,44 +1,72 @@
 /*
-  IITC exact search
+  IITC accent-less search (diacritic insensitive).
   
-  Adds exact matches with "==" in title.
+  Adds insenstive matches.
   
   (run once in console)
 */
-function exactSearch(query) {
-	var term = query.term;
-	var teams = ['NEU', 'RES', 'ENL'];
-	console.log('exact search', query);
+IitcAccentLessSearch = class {
+	constructor() {
+		this.svg = `<svg xmlns:xlink="http://www.w3.org/1999/xlink" xmlns="http://www.w3.org/2000/svg" width="12" height="12" version="1.1">
+			<g style="fill:white;stroke:none">
+				<path d="m 6,12 -2,-12  4,0 z" />
+				<path d="m 6,12 -4, -8  8,0 z" />
+				<path d="m 6,12 -6, -4 12,0 z" />
+			</g>
+		</svg>`;
+		this.icon = 'data:image/svg+xml;base64,' + btoa(this.svg);
+	}
 
-	$.each(portals, function (guid, portal) {
+	find(query) {
+		var term = query.term;
+		console.log('accent-less search', query);
+
+		$.each(portals, (guid, portal) => {
+			var data = portal.options.data;
+			if (!data.title)
+				return;
+	
+			if (data.title == term) {
+				this.addResult(query, guid, portal);
+			}
+		});
+	}
+
+	addResult(query, guid, portal) {
 		var data = portal.options.data;
-		if (!data.title)
-			return;
+		query.addResult({
+			title: '~= ' + data.title,
+			description: 'No accent match',
+			position: portal.getLatLng(),
+			icon: this.icon,
+			onSelected: this.onSelect(guid, portal),
+		});
+	}
 
-		if (data.title == term) {
-			var team = portal.options.team;
-			var color = team == TEAM_NONE ? '#CCC' : COLORS[team];
-			query.addResult({
-				title: '== ' + data.title,
-				description: teams[team] + ', L' + data.level + ', ' + data.health + '%, ' + data.resCount + ' Resonators',
-				position: portal.getLatLng(),
-				icon: 'data:image/svg+xml;base64,' + btoa('<svg xmlns:xlink="http://www.w3.org/1999/xlink" xmlns="http://www.w3.org/2000/svg" width="12" height="12" version="1.1">\n	<g style="fill:%COLOR%;stroke:none">\n		<path d="m 6,12 -2,-12  4,0 z" />\n		<path d="m 6,12 -4, -8  8,0 z" />\n		<path d="m 6,12 -6, -4 12,0 z" />\n	</g>\n</svg>\n'.replace(/%COLOR%/g, color)),
-				onSelected: function (result, event) {
-					if (event.type == 'dblclick') {
-						zoomToAndShowPortal(guid, portal.getLatLng());
-					} else if (window.portals[guid]) {
-						if (!map.getBounds().contains(result.position))
-							map.setView(result.position);
-						renderPortalDetails(guid);
-					} else {
-						window.selectPortalByLatLng(portal.getLatLng());
-					}
-					return true;
-				},
-			});
-		}
-	});
+	onSelect(guid, portal) {
+		return function (result, event) {
+			if (event.type == 'dblclick') {
+				zoomToAndShowPortal(guid, portal.getLatLng());
+			} else if (window.portals[guid]) {
+				if (!map.getBounds().contains(result.position))
+					map.setView(result.position);
+				renderPortalDetails(guid);
+			} else {
+				window.selectPortalByLatLng(portal.getLatLng());
+			}
+			return true;
+		};
+	}
 }
 
-//removeHook('search', exactSearch);
-addHook('search', exactSearch);
+// remove previous version
+if (typeof accentLessSearch === 'function') {
+	removeHook('search', accentLessSearch);
+}
+
+// add new eaxact search
+var iitcAccentLessSearch = new IitcAccentLessSearch();
+function accentLessSearch(query) {
+	iitcAccentLessSearch.find(query);
+}
+addHook('search', accentLessSearch);
