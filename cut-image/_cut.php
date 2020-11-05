49a7c902f72064763d73b0b16d280befc2af16cd
Eccenux <eccenux+git@gmail.com>
2020-11-05 19:31:49 +0100

    higher limit for larger image

--- cut-image/_cut.php
+++ cut-image/_cut.php
@@ -5,6 +5,7 @@
  * Requires PHP 5.5 or higher (due to using e.g. `imagecrop`).
  */
 date_default_timezone_set("Europe/Warsaw");
+ini_set("memory_limit", "2048M");
 
 require_once "./inc/Cutter.php";
 
