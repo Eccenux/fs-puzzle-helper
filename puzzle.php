e9eac59e10cf9c8e01a7e1bfea27d14cff81984b
Eccenux <eccenux+git@gmail.com>
2020-11-05 21:48:37 +0100

    controls higher

--- puzzle.php
+++ puzzle.php
@@ -16,37 +16,9 @@
 <body>
 	<main>
 		<section id="zoomer">
-			<img src="img-auto-cut/col_1.jpg" />
+			<img src="img-auto-cut/cells/col_001_001.jpg" />
 		</section>
 		<section id="columns">
-			<!-- keyword layout, 19 cols (June 2020) -->
-			<!-- 
-			<section>
-				<section class="column left  " id="col_1"><h2>1</h2><img src="img-auto-cut/col_1.jpg"    /></section>
-				<section class="column left  " id="col_2"><h2>2</h2><img src="img-auto-cut/col_2.jpg"    /></section>
-				<section class="column left  " id="col_3"><h2>3</h2><img src="img-auto-cut/col_3.jpg"    /></section>
-				<section class="column left  " id="col_4"><h2>4</h2><img src="img-auto-cut/col_4.jpg"    /></section>
-				<section class="column left  " id="col_5"><h2>5</h2><img src="img-auto-cut/col_5.jpg"    /></section>
-			</section>
-			<section>
-				<section class="column right " id="col_15"><h2>15</h2><img src="img-auto-cut/col_15.jpg"  /></section>
-				<section class="column right " id="col_16"><h2>16</h2><img src="img-auto-cut/col_16.jpg"  /></section>
-				<section class="column right " id="col_17"><h2>17</h2><img src="img-auto-cut/col_17.jpg"  /></section>
-				<section class="column right " id="col_18"><h2>18</h2><img src="img-auto-cut/col_18.jpg"  /></section>
-				<section class="column right " id="col_19"><h2>19</h2><img src="img-auto-cut/col_19.jpg"  /></section>
-			</section>
-			<section>
-				<section class="column keyword" id="col_6"><h2>6</h2><img src="img-auto-cut/col_6.jpg"   /></section>
-				<section class="column keyword" id="col_7"><h2>7</h2><img src="img-auto-cut/col_7.jpg"   /></section>
-				<section class="column keyword" id="col_8"><h2>8</h2><img src="img-auto-cut/col_8.jpg"   /></section>
-				<section class="column keyword" id="col_9"><h2>9</h2><img src="img-auto-cut/col_9.jpg"   /></section>
-				<section class="column keyword" id="col_10"><h2>10</h2><img src="img-auto-cut/col_10.jpg" /></section>
-				<section class="column keyword" id="col_11"><h2>11</h2><img src="img-auto-cut/col_11.jpg" /></section>
-				<section class="column keyword" id="col_12"><h2>12</h2><img src="img-auto-cut/col_12.jpg" /></section>
-				<section class="column keyword" id="col_13"><h2>13</h2><img src="img-auto-cut/col_13.jpg" /></section>
-				<section class="column keyword" id="col_14"><h2>14</h2><img src="img-auto-cut/col_14.jpg" /></section>
-			</section>
-			-->
 			<section>
 			<?php
 				// params for testing
@@ -74,7 +46,7 @@
 		</section>
 		<section id="controls">
 			<button id="reset-all">reset all</button>
-			<br>No. of portals (per column): <input type="text" value="<?=implode("\t", $rowCounts)?>"/>
+			&nbsp; &bull; &nbsp; No. of portals (per column): <input type="text" value="<?=implode("\t", $rowCounts)?>"/>
 		</section>
 	</main>
 
