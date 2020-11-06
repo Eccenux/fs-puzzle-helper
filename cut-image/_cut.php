52ac360389a5e004f2069c7121709d3fb61e5a16
Eccenux <eccenux+git@gmail.com>
2020-11-06 20:36:57 +0100

    testing off

--- cut-image/_cut.php
+++ cut-image/_cut.php
@@ -10,7 +10,7 @@ ini_set("memory_limit", "2048M");
 require_once "./inc/Cutter.php";
 
 $testing = false;
-$testing = true;
+// $testing = true;
 if (!$testing) {
 	$cutter = new Cutter("raw.jpg", "../img-auto-cut/cells/", "../img-auto-cut/");
 	$cutter->cut();
