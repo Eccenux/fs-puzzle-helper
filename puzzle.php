33f3c1614af0ba35390c93ffe536cc6b24fc4de2
Eccenux <eccenux+git@gmail.com>
2020-11-28 11:46:45 +0100

    column notes

--- puzzle.php
+++ puzzle.php
@@ -205,8 +205,11 @@
 			<?php
 				for ($column=$startCol; $column <= $colCount; $column++) {
 					$files = $rowFiles[$column];
-					//echo '<section class="column" id="col_'.$column.'"><h2>'.$column.' <em>('.count($files).')</em></h2>';
-					echo '<section class="column" id="col_'.$column.'"><h2>'.$column.'</h2>';
+					echo "
+					<section class='column' id='col_$column'>
+						<h2>$column</h2>
+						<input data-col='$column' name='col_notes' type='text' value=''>
+					";
 					foreach ($files as $file) {
 						$id = preg_replace('#\.\w+$#', '', basename($file));
 						if (preg_match('#.+?0*([0-9]+).+?0*([0-9]+).*#', $id, $matches)) {
