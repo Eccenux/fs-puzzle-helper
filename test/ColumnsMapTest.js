e10163a19ad412a7831c49e91419ea11eed928c8
Eccenux <eccenux+git@gmail.com>
2020-11-30 02:27:05 +0100

    shorter chai assert

--- test/ColumnsMapTest.js
+++ test/ColumnsMapTest.js
@@ -1,4 +1,5 @@
 import chai from 'chai';
+const assert = chai.assert;
 
 import {ColumnsMapViewModel} from '../js/ColumnsMap.js';
 
@@ -12,7 +13,7 @@ describe('columnsMapViewModel', function() {
 		it('Gets points from two sections', function() {
 			var locationLines = `12,34\n\n56,78`;
 			var result = columnsMapViewModel.locationLinesToPoly(locationLines);
-			chai.assert.equal(result.points.length, 2);
+			assert.equal(result.points.length, 2);
 		});
 		it('Gets points from multiple sections', function() {
 			var locationLines = `11,11\n11,12\n\n21,11\n21,12\n\n\n31,11`;
@@ -21,9 +22,9 @@ describe('columnsMapViewModel', function() {
 				.split('\n')
 			;
 			var result = columnsMapViewModel.locationLinesToPoly(locationLines);
-			chai.assert.equal(result.points.length, expected.length);
+			assert.equal(result.points.length, expected.length);
 			var resultPoints = result.points.map(p=>p.join(','));
-			chai.assert.deepEqual(resultPoints, expected);
+			assert.deepEqual(resultPoints, expected);
 		});
 
 		//
@@ -31,15 +32,15 @@ describe('columnsMapViewModel', function() {
 		it('Missing empty for single section', function() {
 			var locationLines = `11,11\n11,12\n21,11\n21,12\n31,11`;
 			var result = columnsMapViewModel.locationLinesToPoly(locationLines);
-			chai.assert.equal(result.missing.length, 0);
+			assert.equal(result.missing.length, 0);
 		});
 		it('Builds missing for sections', function() {
 			var locationLines = `11,11\n11,12\n21,11\n\n21,12\n31,11`;
 			var result = columnsMapViewModel.locationLinesToPoly(locationLines);
-			chai.assert.equal(result.missing.length, 1);
+			assert.equal(result.missing.length, 1);
 			var locationLines = `11,11\n11,12\n\n21,11\n\n21,12\n31,11`;
 			var result = columnsMapViewModel.locationLinesToPoly(locationLines);
-			chai.assert.equal(result.missing.length, 2);
+			assert.equal(result.missing.length, 2);
 		});
 		it('Missing must have two points', function() {
 			var locationLines = `11,11\n11,12\n\n21,11\n\n21,12\n31,11`;
@@ -47,7 +48,7 @@ describe('columnsMapViewModel', function() {
 			for (var index = 0; index < result.missing.length; index++) {
 				const missing = result.missing[index];
 				console.log(index, missing);
-				chai.assert.equal(missing.length, 2, `Expect two points @index: ${index}`);
+				assert.equal(missing.length, 2, `Expect two points @index: ${index}`);
 			}
 		});
 		it('Builds missing from start to end', function() {
@@ -57,7 +58,7 @@ describe('columnsMapViewModel', function() {
 				[['21','12'], ['31','11']],
 			];
 			var result = columnsMapViewModel.locationLinesToPoly(locationLines);
-			chai.assert.deepEqual(result.missing, expected);
+			assert.deepEqual(result.missing, expected);
 		});
 
 		//
@@ -65,13 +66,13 @@ describe('columnsMapViewModel', function() {
 		it('Paths skip single points', function() {
 			var locationLines = `11,11\n11,12\n\n21,11\n21,12\n\n\n31,11`;
 			var result = columnsMapViewModel.locationLinesToPoly(locationLines);
-			chai.assert.equal(result.paths.length, 2);
+			assert.equal(result.paths.length, 2);
 			var locationLines = `11,11\n11,12\n\n21,11\n\n\n31,11\n31,12`;
 			var result = columnsMapViewModel.locationLinesToPoly(locationLines);
-			chai.assert.equal(result.paths.length, 2);
+			assert.equal(result.paths.length, 2);
 			var locationLines = `11,11\n11,12\n\n21,11\n21,12\n\n\n31,11\n31,12`;
 			var result = columnsMapViewModel.locationLinesToPoly(locationLines);
-			chai.assert.equal(result.paths.length, 3);
+			assert.equal(result.paths.length, 3);
 		});
 		it('Paths built for each section', function() {
 			var locationLines = `11,11\n11,12\n11,13\n\n21,11\n21,12\n\n\n31,11\n31,12`;
@@ -82,7 +83,7 @@ describe('columnsMapViewModel', function() {
 			var resultPaths = result.paths
 				.map(s=>s.map(p=>p.join(',')))
 			;
-			chai.assert.deepEqual(resultPaths, expected);
+			assert.deepEqual(resultPaths, expected);
 		});
 	});
 });
