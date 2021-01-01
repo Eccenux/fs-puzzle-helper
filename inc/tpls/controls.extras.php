511abafbd261e7e6a4507220e190f1b9cadf5d7f
Eccenux <eccenux+git@gmail.com>
2021-01-02 00:54:34 +0100

    cut checkup

--- inc/tpls/controls.extras.php
+++ inc/tpls/controls.extras.php
@@ -1,3 +1,5 @@
+<input type="button" value="cut checkup" 
+	onclick="document.body.classList.toggle('cut-checkup')"/>
 <p class="text">
 	<label>No. of portals (per column):</label>
 	<input type="text" value="<?=implode("\t", $rowCounts)?>"/>
