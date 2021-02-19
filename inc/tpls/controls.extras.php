0f24d580942fd7aeb25d5af5d4b03b3f39525941
Eccenux <eccenux+git@gmail.com>
2021-02-19 22:38:39 +0100

    Quick fill suggestions (TSV)

--- inc/tpls/controls.extras.php
+++ inc/tpls/controls.extras.php
@@ -4,6 +4,26 @@
 	<label>No. of portals (per column):</label>
 	<input type="text" value="<?=implode("\t", $rowCounts)?>"/>
 </p>
+<p class="text">
+	<label>Quick fill suggestions (TSV):</label>
+	<input type="text" value="" id="quickFillColumnNotes_tsv" />
+	<input type="button" value="fill" onclick="quickFillColumnNotes()"/>
+	<script>
+	// quick paste
+	function quickFillColumnNotes() {
+		let tsv = document.querySelector('#quickFillColumnNotes_tsv')?.value;
+		if (!tsv) {
+			return;
+		}
+		items = tsv.split('\t')
+		Array.from(document.querySelectorAll('[name="col_notes"]')).map((v,index)=>{
+			if (!v.value.length) {
+				v.value=items[index];
+			}
+		})
+	}
+	</script>
+</p>
 <p>
 	&nbsp; <a href="img-auto-cut/all.jpg" target="_blank">all.jpg</a>
 </p>
