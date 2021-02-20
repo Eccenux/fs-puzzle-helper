64bc068c686920fb4d74fcac7b25eec26b2cca93
Eccenux <eccenux+git@gmail.com>
2021-02-20 23:19:06 +0100

    order

--- inc/tpls/controls.extras.php
+++ inc/tpls/controls.extras.php
@@ -1,9 +1,3 @@
-<input type="button" value="cut checkup" 
-	onclick="document.body.classList.toggle('cut-checkup')"/>
-<p class="text">
-	<label>No. of portals (per column):</label>
-	<input type="text" value="<?=implode("\t", $rowCounts)?>"/>
-</p>
 <p class="text">
 	<label>Quick fill suggestions (TSV):</label>
 	<input type="text" value="" id="quickFillColumnNotes_tsv" />
@@ -24,6 +18,12 @@
 	}
 	</script>
 </p>
+<p class="text">
+	<label>No. of portals (per column):</label>
+	<input type="text" value="<?=implode("\t", $rowCounts)?>"/>
+</p>
+<input type="button" value="cut checkup" 
+	onclick="document.body.classList.toggle('cut-checkup')"/>
 <p>
 	&nbsp; <a href="img-auto-cut/all.jpg" target="_blank">all.jpg</a>
 </p>
